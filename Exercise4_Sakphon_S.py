85f033f5fc0f02f1e861c17ef5ee1b2d6857c6fc
SakSav <sakphon.tent@gmail.com>
2020-06-03 21:43:39 +0700

    Create Exercise4_Sakphon_S.py
    Examination scores

---
+++ Exercise4_Sakphon_S.py
@@ -0,0 +1,13 @@
+print("คะแนนสอบชั้นปีที่ 2 เทอม 1")
+print("------Your Scores---------")
+
+FE_score = 88
+GB_score = 99
+IntroCom_score = 66
+ComPro_score = 77
+
+print("Foundation English               :", FE_score)
+print("General Business                 :", GB_score)
+print("Introduction to Computer Systems :", IntroCom_score)
+print("Computer Programming             :", ComPro_score)
+
