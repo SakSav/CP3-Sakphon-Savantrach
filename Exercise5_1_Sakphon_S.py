5ff403172f7009d076cecb2a040f74d5dbb56b12
SakSav <sakphon.tent@gmail.com>
2020-06-06 21:24:47 +0700

    Create Exercise5_1_Sakphon_S.py
    Show math outputs

---
+++ Exercise5_1_Sakphon_S.py
@@ -0,0 +1,7 @@
+Num1 = int(input("Type your number : "))
+Num2 = int(input("Type another number : "))
+
+print(str(Num1),"+" ,str(Num2),"=",Num1+Num2 )
+print(str(Num1),"-" ,str(Num2),"=",Num1-Num2 )
+print(str(Num1),"*" ,str(Num2),"=",Num1*Num2 )
+print(str(Num1),"/" ,str(Num2),"=",Num1/Num2 )
